2784496a19e92e20a5f922291428999aba2611d3
Jose A Peralta <japc1802@hotmail.com>
2022-11-14 18:00:29 -0500

    remove trainer feature

--- js/model/pokemon.model.js
+++ js/model/pokemon.model.js
@@ -1,41 +1,41 @@
 class PokemonModel {
-    constructor() {
-        this.url = `https://pokeapi.co/api/v2/pokemon/`
-        this.paginationAmount = 5
-        this.paginationValue = 0
-        this.pokemonJson = []
-    }
+  constructor() {
+    this.url = `https://pokeapi.co/api/v2/pokemon/`;
+    this.paginationAmount = 6;
+    this.paginationValue = 0;
+    this.pokemonJson = [];
+  }
 
-    getPaginationValue() {
-        return this.paginationValue
-    }
+  getPaginationValue() {
+    return this.paginationValue;
+  }
 
-    getPaginationNextValue() {
-        return (this.paginationValue + this.paginationAmount)
-    }
+  getPaginationNextValue() {
+    return this.paginationValue + this.paginationAmount;
+  }
 
-    getLocalPokemon(id) {
-        for (const pokemon in this.pokemonJson) {
-            if (this.pokemonJson[pokemon].id == id) {
-                return this.pokemonJson[pokemon]
-            }
-        }
+  getLocalPokemon(id) {
+    for (const pokemon in this.pokemonJson) {
+      if (this.pokemonJson[pokemon].id == id) {
+        return this.pokemonJson[pokemon];
+      }
     }
+  }
 
-    setPagination(next) {
-        if (next && this.paginationValue <= 964) {
-            this.paginationValue += this.paginationAmount
-        } if (next === false && this.paginationValue >= this.paginationAmount) {
-            this.paginationValue -= this.paginationAmount
-        } else {
-        }
+  setPagination(next) {
+    if (next && this.paginationValue <= 964) {
+      this.paginationValue += this.paginationAmount;
     }
-
-    async getPokemon(id) {
-        const response = await fetch(`${this.url}${id}`)
-        const pokemon = await response.json()
-        this.pokemonJson = this.pokemonJson.concat(pokemon)
-        return pokemon
+    if (next === false && this.paginationValue >= this.paginationAmount) {
+      this.paginationValue -= this.paginationAmount;
+    } else {
     }
+  }
 
+  async getPokemon(id) {
+    const response = await fetch(`${this.url}${id}`);
+    const pokemon = await response.json();
+    this.pokemonJson = this.pokemonJson.concat(pokemon);
+    return pokemon;
+  }
 }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const trainerApp = new TrainerController(new TrainerModel(), new TrainerView())
-const pokemonApp = new PokemonController(new PokemonModel(), new PokemonView())
-const teamApp = new TeamController (new TeamModel(), new TeamView())
-trainerApp.getTrainer()
-pokemonApp.getPokemons()
+// const trainerApp = new TrainerController(new TrainerModel(), new TrainerView())
+const pokemonApp = new PokemonController(new PokemonModel(), new PokemonView());
+const teamApp = new TeamController(new TeamModel(), new TeamView());
+// trainerApp.getTrainer()
+pokemonApp.getPokemons();
